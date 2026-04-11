db0e750f9b185cceee81611eb061ed10987d51ab
erj-human <ejeffres3@gatech.edu>
2026-04-11 00:04:41 +0000

    Co-authored-by: GabrielLopezTrelles <GabrielLopezTrelles@users.noreply.github.com>

--- WebDevelopmentLab03/pages/DJ_Helper.py
+++ WebDevelopmentLab03/pages/DJ_Helper.py
@@ -10,67 +10,6 @@ st.write("Welcome to DJ Helper! Enter a song and we will return a list of simila
 baseUrl = 'http://ws.audioscrobbler.com/2.0'
 apiKey = "a28edaddcd62a1f9f8ae8100299fbc3b"
 
-###Similar Artists Function:
-
-def getSimilarArtists(inputArtist):
-
-    #Endpoint code:
-
-    addBaseArtisturl = baseUrl + "/?method=artist.getsimilar&artist="
-    urlArtist = inputArtist.replace(" ", "+")
-    endpoint1 = addBaseArtisturl + urlArtist + "&api_key=" + apiKey + "&format=json"
-    response1 = requests.get(endpoint1)
-
-    #Return the list code:
-
-    try:
-        data1 = response1.json()
-        similarArtistList = []
-        for i in data1["similarartists"]["artist"]:
-            if float(i["match"]) <= 1 and float(i["match"]) >= .5:
-                similarArtistList.append(i["name"])
-        return similarArtistList[:10]
-    
-    except:
-        return "Bad response. Try capitalizing the artist name."
-
-###Similar Songs Function:
-
-def getSimilarSongs(inputSong, inputArtist):
-
-    #Endpoint code:
-    
-    urlArtist = inputArtist.replace(" ", "+")
-    addBaseSongurl = baseUrl + "/?method=track.getsimilar&artist=" + urlArtist
-    urlSong = inputSong.replace(" ", "+")
-    endpoint2 = addBaseSongurl + "&track=" + urlSong + "&api_key=" + apiKey + "&format=json"
-    response2 = requests.get(endpoint2)
-
-    #Return the list code:
-
-    try:
-        data2 = response2.json()
-        similarSongList = []
-        for i in data2["similartracks"]["track"]:
-            if float(i["match"]) <= 1 and float(i["match"]) >= .5:
-                if boolExpressionArtist and not boolExpressionPlays:
-                    if float(i["artist"]["name"]) != inputArtist:
-                        similarSongList.append(i["name"])
-                elif not boolExpressionArtist and boolExpressionPlays:
-                    if float(i["playcount"]) >= 100000:
-                        similarSongList.append(i["name"])
-                elif boolExpressionArtist and boolExpressionPlays:
-                    if float(i["artist"]["name"]) != inputArtist:
-                        if float(i["playcount"]) >= 100000:
-                            similarSongList.append(i["name"])
-                else:
-                    similarSongList.append(i["name"])
-                     
-        return similarSongList[:10]
-    
-    except:
-        return "Bad response"
-
 with st.form("survey_form"):
     inputSong = st.text_input("input a song you want to mix", value=None)
     inputArtist = st.text_input("input the artist", value=None)
@@ -78,12 +17,74 @@ with st.form("survey_form"):
     boolExpressionPlays = st.button("Only the hits")
     boolExpressionArtist = st.button("Keep songs from the same artist")
     submitted = st.form_submit_button("Submit Data")
+
+    ###Similar Artists Function:
+
+    def getSimilarArtists(inputArtist):
+
+        #Endpoint code:
+
+        addBaseArtisturl = baseUrl + "/?method=artist.getsimilar&artist="
+        urlArtist = inputArtist.replace(" ", "+")
+        endpoint1 = addBaseArtisturl + urlArtist + "&api_key=" + apiKey + "&format=json"
+        response1 = requests.get(endpoint1)
+
+        #Return the list code:
+
+        try:
+            data1 = response1.json()
+            similarArtistList = []
+            for i in data1["similarartists"]["artist"]:
+                if float(i["match"]) <= 1 and float(i["match"]) >= .5:
+                    similarArtistList.append(i["name"])
+            return similarArtistList[:10]
+        
+        except:
+            return "Bad response. Try capitalizing the artist name."
+
+    ###Similar Songs Function:
+
+    def getSimilarSongs(inputSong, inputArtist):
+
+        #Endpoint code:
+        
+        urlArtist = inputArtist.replace(" ", "+")
+        addBaseSongurl = baseUrl + "/?method=track.getsimilar&artist=" + urlArtist
+        urlSong = inputSong.replace(" ", "+")
+        endpoint2 = addBaseSongurl + "&track=" + urlSong + "&api_key=" + apiKey + "&format=json"
+        response2 = requests.get(endpoint2)
+
+        #Return the list code:
+
+        try:
+            data2 = response2.json()
+            similarSongList = []
+            for i in data2["similartracks"]["track"]:
+                if float(i["match"]) <= 1 and float(i["match"]) >= .5:
+                    if boolExpressionArtist and not boolExpressionPlays:
+                        if float(i["artist"]["name"]) != inputArtist:
+                            similarSongList.append(i["name"])
+                    elif not boolExpressionArtist and boolExpressionPlays:
+                        if float(i["playcount"]) >= 100000:
+                            similarSongList.append(i["name"])
+                    elif boolExpressionArtist and boolExpressionPlays:
+                        if float(i["artist"]["name"]) != inputArtist:
+                            if float(i["playcount"]) >= 100000:
+                                similarSongList.append(i["name"])
+                    else:
+                        similarSongList.append(i["name"])
+                        
+            return similarSongList[:10]
+        
+        except:
+            return "Bad response"
     if submitted:
-        st.write("**You may like these artists:**")
-        for a in getSimilarArtists(inputArtist):
-            st.write(f"{a}")
-        st.write("**You may like these songs:**")
-        for p in getSimilarSongs(inputSong, inputArtist):
-            st.write(f"{p}")
-        st.write(getSimilarSongs(inputSong, inputArtist))
-st.write(sliderNumArtist)
+    st.write("**You may like these artists:**")
+    for a in getSimilarArtists(inputArtist):
+        st.write(f"{a}")
+    st.write("**You may like these songs:**")
+    for p in getSimilarSongs(inputSong, inputArtist):
+        st.write(f"{p}")
+    st.write(getSimilarSongs(inputSong, inputArtist))
+
+
