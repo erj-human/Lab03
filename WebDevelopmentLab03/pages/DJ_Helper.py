8e2d5fffa8a829bbe60d58eddc5c3096bc7d87f9
erj-human <ejeffres3@gatech.edu>
2026-04-11 00:46:30 +0000

    modified:   WebDevelopmentLab03/pages/DJ_Helper.py

--- WebDevelopmentLab03/pages/DJ_Helper.py
+++ WebDevelopmentLab03/pages/DJ_Helper.py
@@ -61,13 +61,13 @@ with st.form("survey_form"):
         for i in data2["similartracks"]["track"]:
             if float(i["match"]) <= 1 and float(i["match"]) >= .5:
                 if boolExpressionArtist and not boolExpressionPlays:
-                    if float(i["artist"]["name"]) != inputArtist:
+                    if i["artist"]["name"] != inputArtist:
                         similarSongList.append(i["name"])
                 elif not boolExpressionArtist and boolExpressionPlays:
                     if int(i["playcount"]) >= 100000:
                         similarSongList.append(i["name"])
                 elif boolExpressionArtist and boolExpressionPlays:
-                    if float(i["artist"]["name"]) != inputArtist:
+                    if i["artist"]["name"] != inputArtist:
                         if int(i["playcount"]) >= 100000:
                             similarSongList.append(i["name"])
             else:
